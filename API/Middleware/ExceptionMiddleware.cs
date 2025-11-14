dea5222c654477e6f7f319330cf2110bf4b6513d
erabeqiriii <ebeqiri@systems3000.com>
2025-11-14 13:49:06 +0100

    End of section 7

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(RequestDelegate next, 
+ILogger<ExceptionMiddleware> logger, IHostingEnvironment env)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "{message}" ,ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = env.IsDevelopment()
+            ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+            : new ApiException(context.Response.StatusCode, ex.Message, "INTERNAL SERVER ERROR");
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
